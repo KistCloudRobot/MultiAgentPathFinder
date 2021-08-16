2acfaa358b19f7c1bd8cb1f0a2b5a49b67732a84
Ahn, Jeeho <jeeho@kist.re.kr>
2021-08-16 17:18:50 +0900

    added ARBI Agent

--- cbs/a_agent.py
+++ cbs/a_agent.py
@@ -2,9 +2,12 @@ from python_arbi_framework.arbi_agent.agent.arbi_agent import ArbiAgent
 from python_arbi_framework.arbi_agent.configuration import BrokerType
 from python_arbi_framework.arbi_agent.agent import arbi_agent_excutor
 
-class arbiAgent(ArbiAgent):
-    def __init__(self):
+class aAgent(ArbiAgent):
+    def __init__(self, agent_name, broker_url = "tcp://127.0.0.1:61616"):
         super().__init__()
+        self.broker_url = broker_url
+        self.agent_name = agent_name
+        #self.agent_url = agent_url
 
     def on_data(self, sender: str, data: str):
         print(self.agent_url + "\t-> receive data : " + data)
@@ -19,8 +22,9 @@ class arbiAgent(ArbiAgent):
     """
     def on_query(self, sender: str, query: str) -> str:
         print(self.agent_url + "\t-> receive query : " + query)
+        #print(query)
         return "(query ok)"
 
-    def execute(self, broker_url, agent_name, agent, broker_type=2):
-        arbi_agent_excutor.excute(broker_url, agent_name, agent, broker_type)
-        print(agent_name + " ready")
+    def execute(self, broker_type=2):
+        arbi_agent_excutor.excute(self.broker_url, self.agent_name, self, broker_type)
+        print(self.agent_name + " ready")
